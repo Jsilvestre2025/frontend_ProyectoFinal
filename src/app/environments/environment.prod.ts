baa40f16805cec74933d444ce84043d6efb49ade
Jsilvestre2025 <s.jeremmy17@gmail.com>
2025-11-07 04:56:10 -0300

    Parche 1.4 Configuracion Conexion Vercel-Render

---
+++ src/app/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  // Pega tu nueva URL de Render aquí
+  apiUrl: 'https://backend-proyectofinal-hhyt.onrender.com/api' 
+};
---
+++ src/app/environments/environment.ts
@@ -0,0 +1,5 @@
+// Este es el archivo para 'ng serve'
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:3000/api' // URL de tu backend local
+};
